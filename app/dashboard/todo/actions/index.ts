c1de0ad25f6fabf8564dd6d5ece974edaa13981b
Prince Prakalp <30505797+algorithm98@users.noreply.github.com>
2024-03-23 00:33:26 +0530

    update 1.1.1

--- app/dashboard/todo/actions/index.ts
+++ app/dashboard/todo/actions/index.ts
@@ -1,12 +1,12 @@
-// "use server";
-// export async function createTodo() {
-// }
-// export async function updateTodoById(id: string) {
-// 	console.log("update todo");
-// }
-// export async function deleteTodoById(id: string) {}
+"use server";
+export async function createTodo() {
+}
+export async function updateTodoById(id: string) {
+	console.log("update todo");
+}
+export async function deleteTodoById(id: string) {}
 
-// export async function readTodos() {}
+export async function readTodos() {}
 
 
 
